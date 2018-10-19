a87525ee4f60ec97b964f457f98b76376966d0b1
Edoardo Gargano <edoardo.gargano@yoox.com>
2018-10-19 19:40:17 +0200

    feature: stores with countries

--- src/store/modules/stores.js
+++ src/store/modules/stores.js
@@ -1,19 +1,30 @@
 import storesJson from "../../../data/store-locator";
+import termsJson from "../../../data/terms";
 
 const state = {
   all: [],
   pending: false,
   error: false,
-  selectedStoreId: 7978
+  selectedStoreId: 7978,
+  countries: [],
+  selectedCountryTermId: 0
 };
 
 const getters = {
   getCenteredStore: state => {
     return state.all.filter(store => store.ID === state.selectedStoreId)[0];
+  },
+  getSelectedCountryTermId: state => {
+    return state.selectedCountryTermId;
   }
 };
 
 const actions = {
+  getCountries({ commit }) {
+    commit("apiPending");
+
+    commit("receiveNations", termsJson);
+  },
   getAllStores({ commit }) {
     commit("apiPending");
 
@@ -32,10 +43,20 @@ const actions = {
   },
   selectStore({ commit }, { id }) {
     commit("selectStore", id);
+  },
+  selectCountryTermId({ commit }, term_id) {
+    commit("selectCountryTermId", term_id);
   }
 };
 
 const mutations = {
+  selectCountryTermId(state, term_id) {
+    state.selectedCountryTermId = term_id;
+  },
+  receiveNations(state, jsonTerms) {
+    state.pending = false;
+    state.countries = terms(jsonTerms.terms).countries;
+  },
   receiveAll(state, stores) {
     state.pending = false;
     state.all = stores;
@@ -59,3 +80,17 @@ export default {
   actions,
   mutations
 };
+
+const terms = terms =>
+  terms.map(term => term).reduce(
+    (acc, cur) => {
+      if (cur.parent === 0) {
+        acc.continents.push(cur.term_id);
+      }
+      if (acc.continents.indexOf(cur.parent) > -1) {
+        acc.countries.push(cur);
+      }
+      return acc;
+    },
+    { continents: [], countries: [] }
+  );
