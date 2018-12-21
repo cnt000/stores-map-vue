e01cd34970cac801515f550ae167b0e5552fedfc
Edoardo Gargano <cnt000@gmail.com>
2018-12-21 15:47:59 +0100

    fix: terms filter helper

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,11 +1,11 @@
-export const terms = terms =>
+export const termsToCountries = terms =>
   terms.map(term => term).reduce(
     (acc, cur) => {
       if (cur.parent === 0) {
         acc.continents.push(cur.term_id);
       }
       if (acc.continents.indexOf(cur.parent) > -1) {
-        acc.countries.push({ cur });
+        acc.countries.push(cur);
       }
       return acc;
     },
