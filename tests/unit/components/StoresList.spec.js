c04fd6441c7bb2eb431cc82a418b1564432d285d
Edoardo Gargano <cnt000@gmail.com>
2019-02-17 18:22:12 +0100

    test: removed select country tests

--- tests/unit/components/StoresList.spec.js
+++ tests/unit/components/StoresList.spec.js
@@ -45,7 +45,7 @@ describe("StoresList.vue", () => {
       store,
       localVue
     });
-    expect(wrapper.find("li:first-child > span").text()).toMatch(msg);
+    expect(wrapper.find("li:first-child > div").text()).toMatch(msg);
   });
 
   it("renders list item BAKU - PORT BAKU - II element", () => {
@@ -54,26 +54,26 @@ describe("StoresList.vue", () => {
       store,
       localVue
     });
-    expect(wrapper.find("li:nth-child(2) > span").text()).toMatch(msg);
+    expect(wrapper.find("li:nth-child(2) > div").text()).toMatch(msg);
   });
 
-  it("renders countries select with All", () => {
-    const msg = "All";
-    const wrapper = shallowMount(StoresList, {
-      store,
-      localVue
-    });
-    expect(wrapper.find("select option").text()).toMatch(msg);
-  });
+  // it("renders countries select with All", () => {
+  //   const msg = "All";
+  //   const wrapper = shallowMount(StoresList, {
+  //     store,
+  //     localVue
+  //   });
+  //   expect(wrapper.find("select option").text()).toMatch(msg);
+  // });
 
-  it("renders countries select with China second option", () => {
-    const msg = "Asia"; // not important if country or continents in this test
-    const wrapper = shallowMount(StoresList, {
-      store,
-      localVue
-    });
-    expect(wrapper.find("select option:nth-child(2)").text()).toMatch(msg);
-  });
+  // it("renders countries select with China second option", () => {
+  //   const msg = "Asia"; // not important if country or continents in this test
+  //   const wrapper = shallowMount(StoresList, {
+  //     store,
+  //     localVue
+  //   });
+  //   expect(wrapper.find("select option:nth-child(2)").text()).toMatch(msg);
+  // });
 
   it("renders one store searching for BAKU - PORT BAKU", () => {
     const msg = "BAKU - PORT BAKU";
@@ -83,7 +83,7 @@ describe("StoresList.vue", () => {
     });
     wrapper.find(".stores_search").element.value = msg;
     wrapper.find(".stores_search").trigger("input");
-    expect(wrapper.find("li:first-child > span").text()).toMatch(msg);
+    expect(wrapper.find("li:first-child > div").text()).toMatch(msg);
   });
 
   it("renders any store searching for AAA", () => {
@@ -94,22 +94,22 @@ describe("StoresList.vue", () => {
     });
     wrapper.find(".stores_search").element.value = msg;
     wrapper.find(".stores_search").trigger("input");
-    expect(wrapper.find("li:first-child > span").text()).toMatch(msg);
+    expect(wrapper.find("li:first-child > div").text()).toMatch(msg);
   });
 
   it('calls store action "selectStore" when list item is clicked', () => {
     const wrapper = shallowMount(StoresList, { store, localVue });
-    const listItem = wrapper.find("li:first-child > span");
+    const listItem = wrapper.find("li:first-child > div");
     listItem.trigger("click");
     expect(actions["stores/selectStore"]).toHaveBeenCalled();
   });
 
-  it('calls store action "selectCountryId" when list item is clicked', () => {
-    const wrapper = shallowMount(StoresList, { store, localVue });
-    const selectCountry = wrapper.find("select");
-    selectCountry.trigger("change", 0);
-    expect(actions["stores/selectCountryId"]).toHaveBeenCalled();
-  });
+  // it('calls store action "selectCountryId" when select change', () => {
+  //   const wrapper = shallowMount(StoresList, { store, localVue });
+  //   const selectCountry = wrapper.find("select");
+  //   selectCountry.trigger("change", 0);
+  //   expect(actions["stores/selectCountryId"]).toHaveBeenCalled();
+  // });
 
   it('calls store action "getCountries" when is created', () => {
     shallowMount(StoresList, { store, localVue });
