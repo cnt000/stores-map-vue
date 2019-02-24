c528c1cd69060e6d4b3090c4097f885f75944262
Edoardo Gargano <cnt000@gmail.com>
2019-02-24 12:42:33 +0100

    feat: map data with rambda pathOr, propOr

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,5 @@
+import * as R from "ramda";
+
 export const termsToCountries = terms =>
   terms.map(term => term).reduce(
     (acc, cur) => {
@@ -12,19 +14,32 @@ export const termsToCountries = terms =>
     { continents: [], countries: [] }
   );
 
+// li potrei prendere con ramda tutti in uo
 export function decodeStores(rawStores) {
   return rawStores.map(store => ({
-    id: String(store.ID) || "",
-    name: store.post_title || "",
-    address: store["wpcf-yoox-store-address"] || "",
-    gender: store["wpcf-store-gender"] || "",
-    lat: store.lat || "",
-    lng: store.lng || "",
-    phone: store["wpcf-yoox-store-phone"] || "",
-    mail: store["wpcf-yoox-store-email"] || "",
-    hours: store["wpcf-yoox-store-hours"] || "",
-    countryIso: store["wpcf-yoox-store-country-iso"] || "",
-    locations: store.location,
+    id: String(R.prop("ID", store)),
+    name: titleCase(store.post_title.toLowerCase()) || "",
+    address: R.propOr("", "wpcf-yoox-store-address", store),
+    gender: R.propOr("", "wpcf-store-gender", store),
+    lat: R.propOr("", "lat", store),
+    lng: R.propOr("", "lng", store),
+    phone: R.propOr("", "wpcf-yoox-store-phone", store),
+    mail: R.propOr("", "wpcf-yoox-store-email", store),
+    hours: R.propOr("", "wpcf-yoox-store-hours", store),
+    countryIso: R.propOr("", "wpcf-yoox-store-country-iso", store),
+    city: R.pathOr("", ["location", "city", "name"], store),
+    country: R.pathOr("", ["location", "country", "name"], store),
+    continent: R.pathOr("", ["location", "continent", "name"], store),
     visible: true
   }));
+}
+
+function titleCase(str) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map(function(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ");
 }
