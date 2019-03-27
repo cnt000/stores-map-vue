222a2d03c8538e8c7650d346873065fd9c70dfd0
Edoardo Gargano <cnt000@gmail.com>
2019-03-27 08:18:37 +0100

    test: clean test and fix

--- tests/unit/stores/modules/stores.spec.js
+++ tests/unit/stores/modules/stores.spec.js
@@ -1,20 +1,16 @@
 import stores from "@/store/modules/stores";
-import { termsToCountries } from "@/helpers";
 import places from "#/alessandrore";
-import termsJson from "#/terms";
+import { decodeStores } from "@/helpers";
 
 describe("Testing Getters", () => {
   const state = {
-    all: [{ ID: 1 }, { ID: 3 }, { ID: 5 }],
-    selectedStoreId: "3",
+    all: [{ id: 1 }, { id: 3 }, { id: 5 }],
+    selectedStoreId: 3,
     selectedCountryId: 6,
     countries: [{ term_id: 2 }, { term_id: 6 }, { term_id: 8 }]
   };
   it("getSelectedStore", () => {
-    expect(stores.getters.getSelectedStore(state)).toEqual({ ID: 3 });
-  });
-  it("getSelectedCountry", () => {
-    expect(stores.getters.getSelectedCountry(state)).toEqual({ term_id: 6 });
+    expect(stores.getters.getSelectedStore(state)).toEqual({ id: 3 });
   });
 });
 
@@ -35,34 +31,22 @@ describe("Testing Actions", () => {
       }
     };
   });
-
-  it("getCountries", () => {
-    stores.actions.getCountries(obj);
-    expect(actionNames).toEqual([
-      { key: "apiPending" },
-      { key: "receiveCountries", payload: termsJson }
-    ]);
-  });
   it("getAllStores", () => {
     stores.actions.getAllStores(obj);
+    let allStores = decodeStores(places);
     expect(actionNames).toEqual([
       { key: "apiPending" },
-      { key: "receiveAll", payload: places }
+      { key: "receiveAll", payload: allStores }
     ]);
   });
   it("selectStore", () => {
     stores.actions.selectStore(obj, payload);
     expect(actionNames).toEqual([{ key: "selectStore", payload: 1 }]);
   });
-  it("selectCountryId", () => {
-    stores.actions.selectCountryId(obj, payload);
-    expect(actionNames).toEqual([{ key: "selectCountryId", payload: 1 }]);
-  });
 });
 
 describe("Testing Mutations", () => {
   let state;
-  let term_id;
   let storeId;
 
   beforeEach(() => {
@@ -73,34 +57,32 @@ describe("Testing Mutations", () => {
       all: [
         {
           name: "ciao - prova prova",
-          ID: 2,
+          id: 2,
           path: ""
         }
       ]
     };
-    term_id = 1;
     storeId = 2;
-  });
-  it("selectCountryId", () => {
-    stores.mutations.selectCountryId(state, term_id);
-    expect(state).toEqual({ ...state, selectedCountryId: 1 });
-  });
-  it("receiveCountries", () => {
-    const apiPendingState = { ...state, pending: true };
-    stores.mutations.receiveCountries(apiPendingState, termsJson);
-    expect(apiPendingState).toEqual({
-      ...apiPendingState,
-      pending: false,
-      countries: termsToCountries(termsJson.terms).countries
-    });
   });
   it("receiveAll", () => {
     const apiPendingState = { ...state, pending: true };
-    stores.mutations.receiveAll(apiPendingState, places);
+    stores.mutations.receiveAll(apiPendingState, [
+      {
+        name: "ciao - prova prova",
+        id: 2,
+        path: ""
+      }
+    ]);
     expect(apiPendingState).toEqual({
       ...apiPendingState,
       pending: false,
-      all: places
+      all: [
+        {
+          name: "ciao - prova prova",
+          id: 2,
+          path: ""
+        }
+      ]
     });
   });
   it("apiPending", () => {
