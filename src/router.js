52bbe507ea80151fd83acc8ee5a7e4a619a1de12
Edoardo Gargano <cnt000@gmail.com>
2019-04-15 22:17:36 +0200

    fix: router with hash

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ import StoreLocator from "./components/StoreLocator.vue";
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
+  // mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
