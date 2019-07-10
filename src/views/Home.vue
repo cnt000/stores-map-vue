605e8f950b43f781910e98f27774a40fb7b2c8f8
Edoardo Gargano <cnt000@gmail.com>
2019-07-10 20:31:10 +0200

    feat: background on splash screen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
-    <router-link to="/store-locator">Store locator</router-link>
-    baseUrl: {{ baseUrl }}
+    <router-link to="/store-locator">
+      <div class="cta">Click to visit Boutique Locator</div>
+      <div class="background"></div>
+    </router-link>
   </div>
 </template>
 
@@ -18,3 +20,27 @@ export default {
   }
 };
 </script>
+
+
+<style lang="scss">
+.background {
+  height: 100vh;
+  background-image: url("https://upload.wikimedia.org/wikipedia/commons/4/48/PRADA_BOUTIQUE_AOYAMA.jpg");
+
+  /* Add the blur effect */
+  filter: blur(2px);
+  -webkit-filter: blur(2px);
+
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+}
+.cta {
+  position: absolute;
+  top: 10px;
+  left: 0;
+  right: 0;
+  z-index: 1;
+}
+</style>
